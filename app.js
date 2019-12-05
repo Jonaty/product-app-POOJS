9d4ee2ba75cabf235a7a30a9e3689805f031652a
Naty <ingh940422@hotmail.com>
2019-12-05 10:47:13 -0600

    Messages are showed on interface

--- app.js
+++ app.js
@@ -11,29 +11,44 @@ class UI {
 	addProduct(product) {
 		let productList = document.getElementById('product-list');
 		let element = document.createElement('div');
+		element.className = 'card text-center mb-4';
 		element.innerHTML = `
-        <div class="card text-center mb-4"> 
                 <div class="card-body">
                 <strong> Producto name: </strong> ${product.name}
                 <strong> Producto price: </strong> ${product.price}
 				<strong> Producto year: </strong> ${product.year}
 				<a href="#" class="btn btn-danger" name="delete">Delete</a>
                 </div>
-        </div>
 		`;
 		productList.appendChild(element);
 		this.resetForm();
 	}
 
-	resetForm(){
+	resetForm() {
 		document.getElementById('product-form').reset();
 	}
 
-	deleteProduct(element){
-		if(element.name === 'delete'){
+	deleteProduct(element) {
+		if (element.name === 'delete') {
 			// to access to its parent elements and delete it (element = product)
-			element.parentElement.parentElement.parentElement.remove();
+			element.parentElement.parentElement.remove();
 		}
+	}
+
+	showMessage(message, cssClass){
+		// Creating div alert for showing message
+		let alert = document.createElement('div');
+		alert.className = `alert alert-${cssClass} mt-4`;
+		alert.appendChild(document.createTextNode(message));
+		// Showing message in DOM
+		let container = document.querySelector('.container');
+		let app = document.querySelector('#app');
+		// To insert 'alert' inside 'container' but before 'app'
+		container.insertBefore(alert, app);
+		// First param is the action to execute and the second param is time it has to wait to execute the first one
+		setTimeout(function (){
+			document.querySelector('.alert').remove();
+		}, 2000);
 	}
 }
 
@@ -44,12 +59,14 @@ function getData(e) {
 	let product = new Product(name, price, year);
 	let ui = new UI();
 	ui.addProduct(product);
+	ui.showMessage('Product added successfully', 'success');
 	e.preventDefault();
 }
 
-function deleteData(e){
+function deleteData(e) {
 	let ui = new UI();
 	ui.deleteProduct(e.target);
+	ui.showMessage('Product deleted successfully', 'info');
 }
 
 // DOM Events
