cd012948078898833163de154b2a81ab7a191eb4
Naty <ingh940422@hotmail.com>
2019-12-04 18:50:06 -0600

    Form created and show products on list

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-alert('Works!');
+class Product {
+	// El constructor es un método que se ejecuta apenas se crea un nuevo objeto utilizando la clase
+	constructor(name, price, year) {
+		// Parametros que van a conformar el objeto Producto
+		this.name = name;
+		this.price = price;
+		this.year = year;
+	}
+}
+class UI {
+	addProduct(product) {
+		let productList = document.getElementById('product-list');
+		let element = document.createElement('div');
+		element.innerHTML = `
+        <div class="card text-center mb-4"> 
+                <div class="card-body">
+                <strong> Producto name: </strong> ${product.name}
+                <strong> Producto price: </strong> ${product.price}
+                <strong> Producto year: </strong> ${product.year}
+                </div>
+        </div>
+		`;
+		productList.appendChild(element);
+		this.resetForm();
+	}
+
+	resetForm(){
+		document.getElementById('product-form').reset();
+	}
+}
+
+function obtenerDatos(e) {
+	let name = document.getElementById('name').value;
+	let price = document.getElementById('price').value;
+	let year = document.getElementById('year').value;
+	let product = new Product(name, price, year);
+	let ui = new UI();
+	ui.addProduct(product);
+	e.preventDefault();
+}
+
+// DOM Events
+document.getElementById('product-form').addEventListener('submit', obtenerDatos);
